dae692e341cd4f7f3e5dd48100d3b4be0383a7dd
JON1MAN <117488279+JON1MAN@users.noreply.github.com>
2024-08-02 00:23:03 +0300

    adding users component and images to login register

---
+++ frontend/web-gym-app/src/app/components/users/users.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { AxiosService } from '../../axios.service';
+import { User } from '../../interfaces/user';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgFor } from '@angular/common';
+import { CarouselModule } from 'primeng/carousel';
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [
+    NgFor,
+    CarouselModule
+  ],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css'
+})
+export class UsersComponent implements OnInit{
+  public users!: User[];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  public getUsers(): void{
+    this.userService.getAllUsers().subscribe(
+      (response: User[]) => {
+        this.users = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+}
