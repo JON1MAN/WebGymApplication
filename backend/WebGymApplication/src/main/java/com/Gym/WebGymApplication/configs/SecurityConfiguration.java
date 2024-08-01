a2d96c9c88dc7c61e3ba1530fcab1de12c2cc77e
JON1MAN <117488279+JON1MAN@users.noreply.github.com>
2024-08-02 00:39:50 +0300

    added 401 UNAUTHORIZED STATUS

--- backend/WebGymApplication/src/main/java/com/Gym/WebGymApplication/configs/SecurityConfiguration.java
+++ backend/WebGymApplication/src/main/java/com/Gym/WebGymApplication/configs/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -42,6 +44,8 @@ public class SecurityConfiguration {
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
+        http.exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+
         return http.build();
     }
 
@@ -50,7 +54,7 @@ public class SecurityConfiguration {
 //        CorsConfiguration configuration = new CorsConfiguration();
 //
 //        configuration.setAllowedOrigins(List.of(
-//                "http://localhost:8080"
+//                "http://localhost:4200"
 //        ));
 //        configuration.setAllowedMethods(List.of(
 //                "GET", "POST"
