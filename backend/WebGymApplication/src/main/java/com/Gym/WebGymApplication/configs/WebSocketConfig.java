9af20ba45c12c4726e1ce847fc0248bac1f75bc7
JON1MAN <117488279+JON1MAN@users.noreply.github.com>
2024-08-05 12:32:35 +0300

    websocket config and ChatController

---
+++ backend/WebGymApplication/src/main/java/com/Gym/WebGymApplication/configs/WebSocketConfig.java
@@ -0,0 +1,33 @@
+package com.Gym.WebGymApplication.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic",  "/queue");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket")
+                .setAllowedOrigins("http://localhost:4200")
+                .withSockJS();
+    }
+
+}
--- backend/WebGymApplication/src/main/java/com/Gym/WebGymApplication/controllers/ChatController.java
+++ backend/WebGymApplication/src/main/java/com/Gym/WebGymApplication/controllers/ChatController.java
@@ -1,2 +1,30 @@
-package com.Gym.WebGymApplication.controllers;public class ChatController {
+package com.Gym.WebGymApplication.controllers;
+
+import com.Gym.WebGymApplication.models.Message;
+import com.Gym.WebGymApplication.services.MessageService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@Controller
+public class ChatController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @MessageMapping("/chats/{chatId}")
+    @SendTo("/topic/chats/{chatId}")
+    public Message sendMessage(
+            @Payload Message message,
+            @PathVariable("chatId") String chatId
+    ){
+        return messageService.sendMessageToChat(message);
+    }
+
 }
