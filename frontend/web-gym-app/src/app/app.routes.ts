9087bbae0f11b5fee234faebd290d974a7ab2616
JON1MAN <117488279+JON1MAN@users.noreply.github.com>
2024-08-10 00:43:55 +0300

    added routes for frontend components

--- frontend/web-gym-app/src/app/app.routes.ts
+++ frontend/web-gym-app/src/app/app.routes.ts
@@ -1,3 +1,47 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { MembershipsComponent } from './components/memberships/memberships.component';
+import { BuymembershipComponent } from './components/buymembership/buymembership.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'users',
+        component: UsersComponent
+    },
+    {
+        path: 'chats',
+        component: ChatComponent
+    },
+    {
+        path: 'logout',
+        component: LogoutComponent
+    },
+    {
+        path: 'memberships',
+        component: MembershipsComponent
+    },
+    {
+        path: 'buymembership',
+        component: BuymembershipComponent
+    },
+    {
+        path: '', redirectTo: 'home', pathMatch: 'full'
+    }
+];
