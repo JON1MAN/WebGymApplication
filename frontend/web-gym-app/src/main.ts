dd6994578e721e6d5c107763b770f5785713c51b
JON1MAN <117488279+JON1MAN@users.noreply.github.com>
2024-08-10 00:44:48 +0300

    added main.ts

--- frontend/web-gym-app/src/main.ts
+++ frontend/web-gym-app/src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
+import { authInterceptor, AuthInterceptor } from './app/auth-interceptor';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes), 
+    provideHttpClient(withInterceptors([authInterceptor]))
+  ]
+})
   .catch((err) => console.error(err));
