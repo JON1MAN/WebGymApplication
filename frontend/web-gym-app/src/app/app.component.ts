48615a2339b3b81c771203706a23d80ff15037b1
JON1MAN <117488279+JON1MAN@users.noreply.github.com>
2024-08-14 00:51:37 +0300

    app component

--- frontend/web-gym-app/src/app/app.component.ts
+++ frontend/web-gym-app/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from './interfaces/user';
+import { UserService } from './services/user.service';
+import { response } from 'express';
+import { error } from 'console';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +14,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
   title = 'web-gym-app';
 }
--- frontend/web-gym-app/src/app/app.config.ts
+++ frontend/web-gym-app/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
